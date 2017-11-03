b9611751c195696f7c5f1e7b6d8afdd4a758cf0e
Esau <1165050143@qq.com>
2017-11-03 15:40:05 +0900

    compute pre-path

--- src/exercise/partialtree/constructor/PartialTreesBuilder.java
+++ src/exercise/partialtree/constructor/PartialTreesBuilder.java
@@ -33,16 +33,74 @@ public class PartialTreesBuilder {
 		
 		List<List<Tag>> chunkList=getChunksByXML(xmlDoc, treeNum);
 		
-		List<List<Node>> subTreeList=buildSubTrees(chunkList);
+		List<List<Node>> subTreeLists=buildSubTrees(chunkList);
 		
 		return null;
 	}
 	
+	private List<Node> getPrePath(List<List<Node>> sts){
+
+		List<List<Node>> lls=new ArrayList<>();
+		List<List<Node>> rls=new ArrayList<>();
+		
+		// open nodes in LLS or RLS are arranged in top-bottom order
+		int p=sts.size();
+		for(int i=0; i<p; i++) {
+			lls.add(selectLeftOpenNode(sts.get(i)));
+			rls.add(selectRightOpenNode(sts.get(i)));
+		}
+		
+		Deque<Node> auxList=new ArrayDeque<>();
+		
+		for(int i=0;i<p-1;i++) {
+			List<Node> rl=rls.get(i);
+			for(int j=0;j<rl.size();j++) {
+				
+			}
+		}
+		
+		
+		return null;
+	}
+	
+	private List<Node> selectLeftOpenNode(List<Node> pt){
+		
+		Node p=pt.get(0);
+		
+		List<Node> ll=new ArrayList<>();
+		
+		while(p!=null&&NodeType.LEFT_OPEN_NODE.equals(p.getType())) {
+			ll.add(p);
+			System.out.print(" +"+p.getTagName()+p.getUid()+" ");
+			p=p.getChildList().get(0);
+		}
+		
+		return ll;
+	}
+	
+	private List<Node> selectRightOpenNode(List<Node> pt){	
+		
+		Node p=pt.get(pt.size()-1);
+		
+		List<Node> rl=new ArrayList<>();
+		
+		while(p!=null&&NodeType.RIGHT_OPEN_NODE.equals(p.getType())) {
+			rl.add(p);
+			System.out.print(" +"+p.getTagName()+p.getUid()+" ");
+			List<Node> childList=p.getChildList();
+			p=childList.get(childList.size()-1);
+		}
+		
+		return rl;
+	}
+	
 	private List<List<Node>> buildSubTrees( List<List<Tag>> chunkList ){
 		
+		List<List<Node>> subTreeLists=new ArrayList<List<Node>>();
 		for(int i=0;i<chunkList.size();i++) {
 			List<Node> subTrees = buildTreesByTags(chunkList.get(i));
-			System.out.println("chunk"+i+" : ");
+			subTreeLists.add(subTrees);
+			System.out.println("pt "+i+" : ");
 			for(Node root: subTrees) {
 				bfs(root);
 			}
@@ -125,57 +183,6 @@ public class PartialTreesBuilder {
 		
 		return stack.pop().getChildList();
 	}
-	
-	
-	private Node _buildTreeByTags(List<Tag> tagList) {
-		
-		Deque<Tag> stack = new ArrayDeque<Tag>();
-		
-		Deque<Node> nodeList=new ArrayDeque<Node>();
-		
-		int uid=0;
-		
-		for(int i=0;i<tagList.size();i++) {
-			Tag tag=tagList.get(i);
-			if(tag.getType().equals(TagType.START)) {
-				stack.push(tag);
-			}else {
-				Tag tag2=stack.peek();
-				if(tag2!=null && tag2.getName().equals(tag.getName())) {
-					stack.pop();
-					Node node=NodeFactory.createNode(tag.getName(), NodeType.CLOSED_NODE, uid++);
-					nodeList.addLast(node);
-				}else {
-					Node node=NodeFactory.createNode(tag.getName(), NodeType.LEFT_OPEN_NODE, uid++);
-					nodeList.addLast(node);
-				}
-			}
-		}
-		
-		while(!stack.isEmpty()) {
-			Tag tag=stack.pop();
-			Node node=NodeFactory.createNode(tag.getName(), NodeType.RIGHT_OPEN_NODE, uid++);
-			nodeList.addLast(node);
-		}
-		
-		while(!nodeList.isEmpty()) {
-			Node node=nodeList.removeFirst();
-			if(node.getType().equals(NodeType.CLOSED_NODE)) {
-				System.out.print(node.getTagName()+node.getUid()+"  ");
-			}
-			if(node.getType().equals(NodeType.LEFT_OPEN_NODE)) {
-				System.out.print("+"+node.getTagName()+node.getUid()+"  ");
-			}
-			if(node.getType().equals(NodeType.RIGHT_OPEN_NODE)) {
-				System.out.print(node.getTagName()+node.getUid()+"+  ");
-			}
-		}
-		
-		System.out.println();
-		
-		return null;
-	}
-	
 
 	private List<List<Tag>> getChunksByXML(String xmlDoc, int chunkNum){
 		
@@ -191,15 +198,8 @@ public class PartialTreesBuilder {
 		}
 		pos[chunkNum]=xmlDoc.length();
 		
-//		String[] chunks=new String[chunkNum];
-//		for(int i=0;i<chunkNum;i++) {
-//			chunks[i]=xmlDoc.substring(pos[i], pos[i+1]);
-//			System.out.println("chunks"+i+" : "+chunks[i]);
-//		}
-		
 		Deque<Tag> allTags=new ArrayDeque<>();
 		for(int i=0;i<chunkNum;i++) {
-//			System.out.print("chunks"+i+" : ");
 			List<Tag> list = getTagsByXML( xmlDoc.substring(pos[i], pos[i+1]) );
             chunkList.add( list );
             for(int j=0; j<list.size(); j++) {
@@ -220,8 +220,11 @@ public class PartialTreesBuilder {
 		}
 		
 		
-		for(List<Tag> list: chunkList) {
-			for(Tag tag: list) {
+		for(int i=0;i<chunkNum;i++) {
+			System.out.print("Chunk"+i+" : ");
+			List<Tag> list = chunkList.get(i);
+			for(int j=0; j<list.size(); j++) {
+				Tag tag = list.get(j);
 				if(tag.getType().equals(TagType.START)) {
 					System.out.print("  <"+tag.getName()+">");
 				}else {
@@ -230,14 +233,13 @@ public class PartialTreesBuilder {
 			}
 			System.out.println();
 		}
+		System.out.println();
 		
 		
 		return chunkList;
 	}
 	
 	private List<Tag> getTagsByXML(String chunkStr){
-
-//		System.out.println(chunkStr);
 		
 		List<Tag> tagList=new ArrayList<Tag>();
 
@@ -265,53 +267,6 @@ public class PartialTreesBuilder {
 		
 	}
 	
-	
-	private List<List<Tag>> _getChunksByXML(String xmlDoc, int chunkNum){
-		
-		String tem=xmlDoc.substring(1, xmlDoc.length()-1);
-		
-		String[] tagsArr=tem.split("><");
-		
-//		for(String)
-		
-		List<List<Tag>> chunkList=new ArrayList<List<Tag>>();
-
-		for(int i=0;i<chunkNum;i++) {
-			chunkList.add(new ArrayList<Tag>());
-		}
-		
-		int tagNum=(int)Math.ceil((double)tagsArr.length/chunkNum);
-		for(int i=0;i<tagNum;i++) {		
-			for(int j=0;j<chunkNum;j++) {
-				if(i+j*tagNum>=tagsArr.length) {
-					break;
-				}
-				String s=tagsArr[i+j*tagNum].trim();
-				Tag tag=new Tag();
-				if(s.contains("/")) {
-					tag.setName(s.substring(s.indexOf('/')).trim());
-					tag.setType(TagType.START);
-				}else {
-					tag.setName(s);
-					tag.setType(TagType.START);
-				}
-				chunkList.get(j).add(tag);
-			}			
-		}
-		
-		for(List<Tag> list: chunkList) {
-			for(Tag tag: list) {
-				if(tag.getType().equals(TagType.START)) {
-					System.out.print("  <"+tag.getName()+">");
-				}else {
-					System.out.print("  /<"+tag.getName()+">");
-				}
-			}
-			System.out.println();
-		}
-		
-		return null;
-	}
 	
 }
 
