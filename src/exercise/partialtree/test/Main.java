58614b7789ae43d7ebdda57e0c191d5f314dde1f
Esau <1165050143@qq.com>
2017-11-04 13:14:49 +0900

    test case

--- src/exercise/partialtree/test/Main.java
+++ src/exercise/partialtree/test/Main.java
@@ -13,11 +13,13 @@ public class Main {
 //				                                                + "</E></D></B><E><D></D></E><B>"
 //				                                                + "<D></D><C></C></B><B></B></A>", 5);
 
-		PartialTreesBuilder.getInstance().createPartialTreesByXML("<A><B><C><E></E></C><D></D>"
-				                                                + "</B><E></E><B><B><D><E></E></D>"
-				                                                + "<C></C></B><C><E></E></C><D><E>"
-				                                                + "</E></D></B><E><D></D></E><B>"
-				                                                + "<D></D><C></C></B><B></B></A>", 5);
+//		PartialTreesBuilder.getInstance().createPartialTreesByXML("<A><B><C><E></E></C><D></D>"
+//				                                                + "</B><E></E><B><B><D><E></E></D>"
+//				                                                + "<C></C></B><C><E></E></C><D><E>"
+//				                                                + "</E></D></B><E><D></D></E><B>"
+//				                                                + "<D></D><C></C></B><B></B></A>", 5);
+
+		PartialTreesBuilder.getInstance().createPartialTreesByXML("<A><B><C><D></D></C></B></A>", 3);
 		
 	}
 
